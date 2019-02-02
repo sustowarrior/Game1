6424c392e4472071f5e0409ed1204ec45f732d2a
Felipe <felipejhony91@gmail.com>
2019-02-02 12:37:22 -0200

    sucess in implement encryption for file, will use it to save properties of all sorts

--- TextGame/src/BinaryUtils.java
+++ TextGame/src/BinaryUtils.java
@@ -1,6 +1,109 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
 public class BinaryUtils {
 
-	final static String FILE_NAME = "C:\\Users\\DEV\\Desktop\\binary\\binary1.kkk";
-	final static String OUTPUT_FILE_NAME = "C:\\Users\\DEV\\Desktop\\binary\\binary2.kkk";
+	final static String FILE_1 = "C:\\Users\\DEV\\Desktop\\binary\\binary1.txt";
+	final static String FILE_2 = "C:\\Users\\DEV\\Desktop\\binary\\binary2.kkk";
+	final static String FILE_3 = "C:\\Users\\DEV\\Desktop\\binary\\binary3.txt";
+
+	public static void init(String msg) {
+		String chave = "This is a secret";
+		
+		File encryptedFile = new File(FILE_2);
+
+		try {
+			
+			write(Cipher.ENCRYPT_MODE, chave, msg, encryptedFile);
+			String ler = read(Cipher.DECRYPT_MODE, chave, encryptedFile);
+			
+			System.out.println("Estava escrito: "+ler);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void process(int cipherMode, String key, File inputFile, File outputFile) {
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(cipherMode, secretKey);
+
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			byte[] inputBytes = new byte[(int) inputFile.length()];
+			inputStream.read(inputBytes);
+
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			outputStream.write(outputBytes);
+
+			inputStream.close();
+			outputStream.close();
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void write(int cipherMode, String key, String msg, File outputFile) {
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(cipherMode, secretKey);
+
+			byte[] inputBytes = msg.getBytes();
+
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			outputStream.write(outputBytes);
+
+			outputStream.close();
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String read(int cipherMode, String key, File inputFile) {
+		
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(cipherMode, secretKey);
+
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			byte[] inputBytes = new byte[(int) inputFile.length()];
+			inputStream.read(inputBytes);
+
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+			String retorno = new String(outputBytes);
+
+			inputStream.close();
+			
+			return retorno;
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 
 }
--- TextGame/src/Launcher.java
+++ TextGame/src/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
+					BinaryUtils.init("Mais um teste");					
 //					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 //					Principal window = new Principal();
 //					window.setVisible(true);
