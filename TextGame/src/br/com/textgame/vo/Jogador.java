e0004f39c38f446bbae93d4dfa8483044676aa69
Felipe <felipejhony91@gmail.com>
2019-02-05 19:40:17 -0200

    forgot to commit some classes

--- TextGame/src/br/com/textgame/vo/Jogador.java
+++ TextGame/src/br/com/textgame/vo/Jogador.java
@@ -1,5 +1,17 @@
 package br.com.textgame.vo;
 
 public class Jogador {
+	private Integer vida;
 
+	public Jogador(Integer vida) {
+		this.vida = vida;
+	}
+
+	public Integer getVida() {
+		return vida;
+	}
+
+	public void setVida(Integer vida) {
+		this.vida = vida;
+	}
 }
