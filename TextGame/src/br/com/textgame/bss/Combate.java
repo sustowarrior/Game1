cb6adb0ea092529abc37d676054eafeed54bf265
Felipe <felipejhony91@gmail.com>
2019-02-05 19:40:34 -0200

    forgot to commit some classes

---
+++ TextGame/src/br/com/textgame/bss/Combate.java
@@ -0,0 +1,18 @@
+package br.com.textgame.bss;
+
+import br.com.textgame.vo.Jogador;
+import br.com.textgame.vo.Monstro;
+
+public class Combate {
+
+	public static String jogadorAtaca(Monstro vidaMonstro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String monstroAtaca(Jogador jogador) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ TextGame/src/br/com/textgame/vo/Monstro.java
@@ -0,0 +1,17 @@
+package br.com.textgame.vo;
+
+public class Monstro {
+	private Integer vida;
+
+	public Monstro(Integer vida) {
+		this.vida = vida;
+	}
+
+	public Integer getVida() {
+		return vida;
+	}
+
+	public void setVida(Integer vida) {
+		this.vida = vida;
+	}
+}
